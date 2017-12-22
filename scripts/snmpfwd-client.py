e8900683629f34172a5437e92765702688556eac
Ilya Etingof <etingof@gmail.com>
2017-12-22 12:05:56 +0100

    stronger crypto protocols support added

--- scripts/snmpfwd-client.py
+++ scripts/snmpfwd-client.py
@@ -47,6 +47,10 @@ PLUGIN_API_VERSION = 2
 authProtocols = {
   'MD5': config.usmHMACMD5AuthProtocol,
   'SHA': config.usmHMACSHAAuthProtocol,
+  'SHA224': config.usmHMAC128SHA224AuthProtocol,
+  'SHA256': config.usmHMAC192SHA256AuthProtocol,
+  'SHA384': config.usmHMAC256SHA384AuthProtocol,
+  'SHA512': config.usmHMAC384SHA512AuthProtocol,
   'NONE': config.usmNoAuthProtocol
 }
 
@@ -56,7 +60,9 @@ privProtocols = {
   'AES': config.usmAesCfb128Protocol,
   'AES128': config.usmAesCfb128Protocol,
   'AES192': config.usmAesCfb192Protocol,
+  'AES192BLMT': config.usmAesBlumenthalCfb192Protocol,
   'AES256': config.usmAesCfb256Protocol,
+  'AES256BLMT': config.usmAesBlumenthalCfb256Protocol,
   'NONE': config.usmNoPrivProtocol
 }
 
--- scripts/snmpfwd-server.py
+++ scripts/snmpfwd-server.py
@@ -46,6 +46,10 @@ CONFIG_FILE = '/usr/local/etc/snmpfwd/server.cfg'
 authProtocols = {
   'MD5': config.usmHMACMD5AuthProtocol,
   'SHA': config.usmHMACSHAAuthProtocol,
+  'SHA224': config.usmHMAC128SHA224AuthProtocol,
+  'SHA256': config.usmHMAC192SHA256AuthProtocol,
+  'SHA384': config.usmHMAC256SHA384AuthProtocol,
+  'SHA512': config.usmHMAC384SHA512AuthProtocol,
   'NONE': config.usmNoAuthProtocol
 }
 
@@ -55,7 +59,9 @@ privProtocols = {
   'AES': config.usmAesCfb128Protocol,
   'AES128': config.usmAesCfb128Protocol,
   'AES192': config.usmAesCfb192Protocol,
+  'AES192BLMT': config.usmAesBlumenthalCfb192Protocol,
   'AES256': config.usmAesCfb256Protocol,
+  'AES256BLMT': config.usmAesBlumenthalCfb256Protocol,
   'NONE': config.usmNoPrivProtocol
 }
 
