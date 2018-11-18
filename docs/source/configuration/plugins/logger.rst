a6b9a253b61eb837ef7d3f6f2f6ab6db9b008ab8
Ilya Etingof <etingof@gmail.com>
2018-11-18 16:37:01 +0100

    Improve logging in logger module
    Added `snmpfwd`, `syslog` and `null` logging methods. Documentation
    updated.

--- docs/source/configuration/plugins/logger.rst
+++ docs/source/configuration/plugins/logger.rst
@@ -36,12 +36,22 @@ options in form of *section.option*.
 *general.method*
 ++++++++++++++++
 
-The logging method: either *file* or *syslog*.
+The logging method:
+
+* snmpfwd - log through main snmpfwd process (default)
+* file - log into a local file
+* syslog - log through local or remote syslog service
+* null - inhibit any logging
+
+*general.level*
++++++++++++++++
+
+Minimal log level to log messages at. Valid values are *debug*, *info* and *error*. Default is *info*.
 
 *file.destination*
 ++++++++++++++++++
 
-Sets path to log file.
+Sets path to a log file.
 
 *file.rotation*
 +++++++++++++++
@@ -51,7 +61,7 @@ Sets the criterion for log file rotation. Valid value is *timed*.
 *file.backupcount*
 ++++++++++++++++++
 
-Sets the limit for rotated files being kept on the filesystem.
+Sets the limit for rotated files to keep on the filesystem. Default is *30*.
 
 *file.timescale*
 ++++++++++++++++
@@ -62,29 +72,59 @@ file rotation. Valid values are:
 * S - *file.interval* is measured in seconds
 * M - *file.interval* is measured in minutes
 * H - *file.interval* is measured in hours
-* D - *file.interval* is measured in days
+* D - *file.interval* is measured in days (default)
 
 *file.interval*
 +++++++++++++++
 
 Together with the *file.timescale* option defines the frequency of
-file rotation.
-
-*syslog.facility*
-+++++++++++++++++
-
-Sets the syslog service facility to use for messages being generated.
-
-*syslog.priority*
-+++++++++++++++++
-
-Sets syslog service priority for the messages being generated. Valid values are *DEBUG*,
-*INFO*, *NOTICE*, *ERROR*, *CRITICAL*.
+file rotation. Default is *1*.
 
 *syslog.transport*
 ++++++++++++++++++
 
-Sets network transport to use by the syslog client to talk to the syslog service.
+Use either *udp* (default) or *tcp* transport for syslog messages.
+
+*syslog.facility*
++++++++++++++++++
+
+Use this syslog service facility. Valid values are:
+
+* *auth*
+* *cron*
+* *daemon* (default)
+* *ftp*
+* *kern*
+* *lpr*
+* *mail*
+* *news*
+* *user*
+* *uucp*
+* *local0* .. *local7*
+
+*syslog.priority*
++++++++++++++++++
+
+Use this syslog service priority. Valid values are:
+
+* Emergency
+* Alert
+* Critical
+* Error
+* Warning
+* Notice
+* Info (default)
+* Debug
+
+*syslog.host*
++++++++++++++
+
+Use syslog service running on *host*. Default is *localhost*.
+
+*syslog.port*
++++++++++++++
+
+Use syslog service running on *host* at port *port*. Default is *514*.
 
 *content.pdus*
 ++++++++++++++
@@ -97,14 +137,28 @@ Sets SNMP PDU types to process. Non-matching PDUs will not be logged. Valid PDU 
 ++++++++++++++++++
 
 Log message template optionally containing `macros`_ to be expanded in the context of
-passing SNMP message.
+passing SNMP message. Additionally to common macros, the following macros specific
+to the `logger` plugin are defined:
+
+* *snmp-var-binds* -- variable-binding pairs that PDU contains
+* *snmp-pdu-type* -- SNMP PDU type
+* *asctime* -- current date/time in default time zone
+* *isotime* -- current date/time in UTC time zone
+* *timestamp* -- current time in seconds from epoch
+* *uptime* -- seconds elapsed since snmpfwd process start
+
+The default is:
+
+.. code-block:: bash
+
+   ${isotime} ${callflow-id} ${snmp-peer-address} ${snmp-pdu-type} ${snmp-var-binds}
 
 *content.parentheses*
 +++++++++++++++++++++
 
 Values in SNMP PDU variable-bindings may contain whitespaces. The *parentheses* option
 may contain two characters or strings which will surround each value in the variable-bindings
-being logged.
+being logged. Default is double quotes (")
 
 .. _logger-macros:
 
--- plugins/logger.py
+++ plugins/logger.py
@@ -10,6 +10,7 @@ import logging
 from logging import handlers
 import time
 import os
+import socket
 import stat
 try:
     from ConfigParser import RawConfigParser, Error
@@ -17,7 +18,7 @@ except ImportError:
     from configparser import RawConfigParser, Error
 from snmpfwd.plugins import status
 from snmpfwd.error import SnmpfwdError
-from snmpfwd.log import debug, info, error
+from snmpfwd import log
 from pysnmp.proto.api import v2c
 
 hostProgs = 'snmpfwd-server', 'snmpfwd-client'
@@ -26,13 +27,42 @@ apiVersions = 0, 2
 
 PLUGIN_NAME = 'logger'
 
-# defaults
-pduMap = {}
-method = 'null'
-logFormat = ''
-leftParen, rightParen = '', ''
+DEFAULTS = {
+    # general
+    'method': 'snmpfwd',
+    'level': 'INFO',
 
+    # file
+    'rotation': 'timed',
+    'backupcount': 30,
+    'timescale': 'D',
+    'interval': 1,
+
+    # syslog
+    'transport': 'udp',
+    'facility': 'DAEMON',
+    'host': 'localhost',
+    'port': 514,
+
+    # content
+    'pdus': ('GetRequest GetNextRequest SetRequest '
+             'GetBulkRequest InformRequest SNMPv2Trap Response'),
+    'template': '${isotime} ${callflow-id} ${snmp-peer-address} '
+                '${snmp-pdu-type} ${snmp-var-binds}',
+    'parentheses': '" "'
+}
+
+SYSLOG_TRANSPORTS = {
+    'udp': socket.SOCK_DGRAM,
+    'tcp': socket.SOCK_STREAM
+}
+
+PDU_MAP = {}
+
+# NOTE(etingof): this is a root logger
 logger = logging.getLogger('snmpfwd-logger')
+
+config = RawConfigParser(defaults=DEFAULTS)
 
 for moduleOption in moduleOptions:
 
@@ -42,97 +72,140 @@ for moduleOption in moduleOptions:
 
         configFile = optionValue
 
-        config = RawConfigParser()
         config.read(configFile)
 
-        method = config.get('general', 'method')
-        if method == 'file':
+method = config.get('general', 'method')
+if method == 'file':
 
-            rotation = config.get('file', 'rotation')
+    rotation = config.get('file', 'rotation')
 
-            if rotation == 'timed':
+    if rotation == 'timed':
 
-                class TimedRotatingFileHandler(handlers.TimedRotatingFileHandler):
-                    """Store log creation time in a stand-alone file''s mtime"""
-                    def __init__(self, *args, **kwargs):
-                        handlers.TimedRotatingFileHandler.__init__(self, *args, **kwargs)
+        class TimedRotatingFileHandler(handlers.TimedRotatingFileHandler):
+            """Store log creation time in a stand-alone file''s mtime"""
+            def __init__(self, *args, **kwargs):
+                handlers.TimedRotatingFileHandler.__init__(self, *args, **kwargs)
 
-                        try:
-                            timestamp = os.stat(self.__filename)[stat.ST_MTIME]
+                try:
+                    timestamp = os.stat(self.__filename)[stat.ST_MTIME]
 
-                        except IOError:
-                            return
+                except IOError:
+                    return
 
-                        # Use a stand-aside file metadata time instead of the last
-                        # modification of the log file itself, as the stock
-                        # implementation does.
-                        # This is to work-around the increasing rotation intervals
-                        # on process restart.
-                        self.rolloverAt = self.computeRollover(timestamp)
+                # Use a stand-aside file metadata time instead of the last
+                # modification of the log file itself, as the stock
+                # implementation does.
+                # This is to work-around the increasing rotation intervals
+                # on process restart.
+                self.rolloverAt = self.computeRollover(timestamp)
 
-                    @property
-                    def __filename(self):
-                        return os.path.join(
-                            os.path.dirname(self.baseFilename),
-                            '.' + os.path.basename(self.baseFilename) + '-timestamp'
-                        )
-
-                    def doRollover(self):
-                        handlers.TimedRotatingFileHandler.doRollover(self)
-
-                        try:
-                            # note log file creation time
-                            open(self.__filename, 'w').close()
-
-                        except IOError:
-                            pass
-
-
-                filename = config.get('file', 'destination')
-
-                handler = TimedRotatingFileHandler(
-                    filename,
-                    config.get('file', 'timescale'),
-                    int(config.get('file', 'interval')),
-                    int(config.get('file', 'backupcount'))
+            @property
+            def __filename(self):
+                return os.path.join(
+                    os.path.dirname(self.baseFilename),
+                    '.' + os.path.basename(self.baseFilename) + '-timestamp'
                 )
 
-            else:
-                raise SnmpfwdError('%s: unknown rotation method %s at %s' % (PLUGIN_NAME, rotation, configFile))
+            def doRollover(self):
+                handlers.TimedRotatingFileHandler.doRollover(self)
 
-        else:
-            raise SnmpfwdError('%s: unknown logging method %s at %s' % (PLUGIN_NAME, method, configFile))
+                try:
+                    # note log file creation time
+                    open(self.__filename, 'w').close()
 
-        debug('%s: using %s logging method' % (PLUGIN_NAME, method))
+                except IOError:
+                    pass
 
-        logger.setLevel(logging.INFO)
 
-        pdus = config.get('content', 'pdus')
-        for pdu in pdus.split():
-            try:
-                pduMap[getattr(v2c, pdu + 'PDU').tagSet] = pdu
+        filename = config.get('file', 'destination')
 
-            except AttributeError:
-                raise SnmpfwdError('%s: unknown PDU %s' % (PLUGIN_NAME, pdu))
-
-            else:
-                debug('%s: PDU ACL includes %s' % (PLUGIN_NAME, pdu))
-
-        try:
-            leftParen, rightParen = config.get('content', 'parentheses').split()
-
-        except Exception:
-            error('%s: malformed "parentheses" values at %s' % (PLUGIN_NAME, configFile))
-
-        template = config.get('content', 'template')
-
-        debug('%s: using var-bind value parentheses %s %s' % (PLUGIN_NAME, leftParen, rightParen))
-
+        handler = TimedRotatingFileHandler(
+            filename,
+            config.get('file', 'timescale'),
+            int(config.get('file', 'interval')),
+            int(config.get('file', 'backupcount'))
+        )
         logger.addHandler(handler)
+
+    else:
+        raise SnmpfwdError('%s: unknown rotation method '
+                           '%s at %s' % (PLUGIN_NAME, rotation, configFile))
+
+elif method == 'syslog':
+
+    try:
+        transport = SYSLOG_TRANSPORTS[config.get('syslog', 'transport')]
+
+    except KeyError:
+        raise SnmpfwdError('%s: unknown syslog transport at %s' % (PLUGIN_NAME, configFile))
+
+    facility = config.get('syslog', 'facility').upper()
+    syslog_host = config.get('syslog', 'host')
+    syslog_port = int(config.get('syslog', 'port'))
+
+    handler = handlers.SysLogHandler(
+        address=(syslog_host, syslog_port),
+        facility=facility,
+        socktype=transport)
+    logger.addHandler(handler)
+
+elif method == 'snmpfwd':
+
+    class ProxyLogger(object):
+        """Just a mock to convey logging calls to snmpfwd log"""
+        error = log.error
+        info = log.info
+        debug = log.debug
+
+        def __str__(self):
+            return PLUGIN_NAME
+
+        def setLevel(self, *args):
+            return
+
+    logger = ProxyLogger()
+
+elif method == 'null':
+    handler = logging.NullHandler()
+    logger.addHandler(handler)
+
+else:
+    raise SnmpfwdError('%s: unknown logging method %s at %s' % (PLUGIN_NAME, method, configFile))
+
+level = config.get('general', 'level').upper()
+
+try:
+    logger.setLevel(getattr(logging, level))
+
+except AttributeError:
+    raise SnmpfwdError('%s: unknown log level %s' % (PLUGIN_NAME, level))
+
+template = config.get('content', 'template')
+
+logger.debug('%s: using %s logging method' % (PLUGIN_NAME, method))
+
+pdus = config.get('content', 'pdus')
+for pdu in pdus.split():
+    try:
+        PDU_MAP[getattr(v2c, pdu + 'PDU').tagSet] = pdu
+
+    except AttributeError:
+        raise SnmpfwdError('%s: unknown PDU %s' % (PLUGIN_NAME, pdu))
+
+    else:
+        logger.debug('%s: PDU ACL includes %s' % (PLUGIN_NAME, pdu))
+
+try:
+    leftParen, rightParen = config.get('content', 'parentheses').split()
+
+except Exception:
+    raise SnmpfwdError('%s: malformed "parentheses" values' % PLUGIN_NAME)
+
+logger.debug('%s: using var-bind value parentheses %s %s' % (PLUGIN_NAME, leftParen, rightParen))
 
 started = time.time()
 
-info('%s: plugin initialization complete' % PLUGIN_NAME)
+logger.info('%s: plugin initialization complete' % PLUGIN_NAME)
 
 
 def _format(template, pdu, context):
@@ -152,7 +225,7 @@ def _format(template, pdu, context):
 
     token = '${snmp-pdu-type}'
     if token in template:
-        template = template.replace(token, pduMap[pdu.tagSet])
+        template = template.replace(token, PDU_MAP[pdu.tagSet])
 
     now = time.time()
 
@@ -181,10 +254,11 @@ def _format(template, pdu, context):
 
 
 def processCommandRequest(pluginId, snmpEngine, pdu, trunkMsg, reqCtx):
-    if pdu.tagSet in pduMap:
+    if pdu.tagSet in PDU_MAP:
         logger.info(_format(template, pdu, trunkMsg))
 
     return status.NEXT, pdu
+
 
 processCommandResponse = processCommandRequest
 
